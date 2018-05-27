2ba8ae3967e982bb2cf567a8d69b11a3637d6426
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-05-26 21:56:09 -0700

    add FactoryMethod example

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FactoryMethod
+{   
+
+    public class Point
+    {
+        private double x, y;
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // factory method
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho*Math.Cos(theta), rho*Math.Sin(theta));
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = Point.NewPolarPoint(1.0, Math.PI / 2);
+            Console.WriteLine(point);
+        }
+    }
+}
