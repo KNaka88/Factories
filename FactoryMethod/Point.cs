3124882cf66d9b882c8175914bc22086b9c6d62c
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-05-28 16:57:24 -0700

    refactor Point

--- FactoryMethod/Point.cs
+++ FactoryMethod/Point.cs
@@ -5,7 +5,7 @@ namespace FactoryMethod
     public class Point
     {
         private double x, y;
-        public Point(double x, double y)
+        private Point(double x, double y)
         {
             this.x = x;
             this.y = y;
@@ -14,6 +14,19 @@ namespace FactoryMethod
         public override string ToString()
         {
             return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+        
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
         }
     }
 }
